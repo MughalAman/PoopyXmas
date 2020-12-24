1ac0505d546e56404836ffe3422ce2bcec2be8d6
MughalAman <60338106+MughalAman@users.noreply.github.com>
2020-12-24 16:45:48 +0200

    Initial commit

---
+++ Assets/Scripts/Santa.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Santa : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private Rigidbody2D rb;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# PoopyXmas💩 🌲🎅🏼🎁❄️
+
+### TODO
+* [x] Get Assets
+* [x] Setup Assets
+* [x] Make Parallax Effect
+* [x] Make Santa move
+* [] Make the poop spawn & drop
+* [] Make poop clickable & add score System
+* [] Add sounds
+* [] Add leaderboard
